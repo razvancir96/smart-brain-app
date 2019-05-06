8cb89a6dcc17f66d0126ef69ee0a658aaefaf6db
razvancir96 <40989335+razvancir96@users.noreply.github.com>
2019-05-06 15:39:44 +0300

    face-recognition-ready

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import FaceRecognitionImage from './components/FaceRecognitionImage/FaceRecognitionImage.js';
 import './App.css';
 
-
 const particlesParams = {
 	particles: {
 		number: {
@@ -20,13 +19,26 @@ const particlesParams = {
 	}
 };
 
+// Clarifai API
+const Clarifai = require('clarifai');
+const app = new Clarifai.App({
+	apiKey: 'b5624aafd619411f8accfc04e257cd73'
+});
+
+
 class App extends Component {
 
 	constructor() {
 		super();
 		this.state = {
 			imageInput: '',
-			url: ''
+			url: '',
+			box: {
+				top: 0,
+				left: 0,
+				bottom: 0,
+				right: 0
+			}
 		}
 	}
 
@@ -35,7 +47,25 @@ class App extends Component {
 	}
 
 	onButtonClick = () => {
-		this.setState({url: this.state.imageInput});
+		// when the submit button is clicked, we pass the url to Clarifai
+		app.models.predict("a403429f2ddf4b49b307e318f00e528b", this.state.imageInput).then(
+		    (response) => {
+				// do something with response
+				const coordinatesObject = response.outputs[0].data.regions[0].region_info.bounding_box;
+				const translatedCoordinatesObject = {
+					top: coordinatesObject.top_row * 100 + '%',
+					left: coordinatesObject.left_col * 100 + '%',
+					bottom: 100 - coordinatesObject.bottom_row * 100 + '%',
+					right: 100 - coordinatesObject.right_col * 100 + '%'
+				}
+				// we set the url state, so the render method is triggered
+				this.setState({url: this.state.imageInput, box: translatedCoordinatesObject});
+		    },
+		    function(err) {
+		      // there was an error
+		      console.log(err);
+		    }
+		);		
 	}
 
 	render() {
@@ -46,7 +76,8 @@ class App extends Component {
 		    <Logo />
 		    <Rank />
 		    <ImageLinkForm inputChange={this.onInputChange} buttonClick={this.onButtonClick}/>
-		    <FaceRecognitionImage url={this.state.url}/>
+		  	{/* we pass the image url and the face recognition box coordinates */}
+		    <FaceRecognitionImage url={this.state.url} boxCoordinates={this.state.box}/>
 		  </div>
 		);
 	}
--- src/components/FaceRecognitionImage/FaceRecognitionImage.js
+++ src/components/FaceRecognitionImage/FaceRecognitionImage.js
@@ -1,44 +1,15 @@
 import React from 'react';
-const Clarifai = require('clarifai');
+import './FaceRecognitionImage.css'
 
-const FaceRecognitionImage = ({url}) => {
-	// initialize with your api key. This will also work in your browser via http://browserify.org/
-	const app = new Clarifai.App({
-		apiKey: 'b5624aafd619411f8accfc04e257cd73'
-	});
-
-	let translatedCoordinatesObject = {
-		top: 0,
-		left: 0,
-		bottom: 0,
-		right: 0
-	}
-
-	app.models.predict("a403429f2ddf4b49b307e318f00e528b", url).then(
-	    function(response) {
-	      // do something with response
-	      console.log(response);
-	      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-	      const coordinatesObject = response.outputs[0].data.regions[0].region_info.bounding_box;
-	      translatedCoordinatesObject = {
-	      	top: coordinatesObject.top_row * 100,
-	      	left: coordinatesObject.left_col * 100,
-	      	bottom: 100 - coordinatesObject.bottom_row * 100,
-	      	right: 100 - coordinatesObject.right_col * 100
-	      }
-	    },
-	    function(err) {
-	      // there was an error
-	      console.log(err);
-	    }
-	);
-
+const FaceRecognitionImage = ({url, boxCoordinates}) => {
 	return(
 		<div>
 			{ url ?
 				(<div className="center ma4">
-					<img src={url} alt="Bad link." className="displayed-image"/>
-					<div className="bounding-box" style={top, bottom, left, right}></div>
+					<div className="absolute">
+						<img src={url} alt="Bad link." className="displayed-image"/>
+						<div className="bounding-box" style={boxCoordinates}></div>
+					</div>
 				</div>) :
 				(<p className="center f3 white">Your image will be displayed here.</p>)
 			}
